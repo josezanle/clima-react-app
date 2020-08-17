689b0a2066e0c6693ba20ec96f4c1c03832726de
josezanle <jose.rios.lm@gmail.com>
2020-08-17 18:53:09 -0300

    primer deploy de la app del clima,con react js

---
+++ precache-manifest.c711588cc6770c0391b3109c670458ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c819cee2f89f924a5aae4db2360625c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27f64d22c7b3499a9721",
+    "url": "/static/css/main.62a5429f.chunk.css"
+  },
+  {
+    "revision": "70162385e7834ba05b0d",
+    "url": "/static/js/2.f39b1333.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f39b1333.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27f64d22c7b3499a9721",
+    "url": "/static/js/main.7852fab3.chunk.js"
+  },
+  {
+    "revision": "1d31edb8731ed6996934",
+    "url": "/static/js/runtime-main.e4ab27e8.js"
+  },
+  {
+    "revision": "8e1da65f4f08b505ca8594e50d22d3ed",
+    "url": "/static/media/imagen.8e1da65f.jpg"
+  }
+]);
